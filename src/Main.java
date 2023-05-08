48b1ffeb6271d915ac4236d02fbf914802f9021e
daniilusacev <2893318@gmail.com>
2023-05-08 13:02:42 +0300

    task1 done

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        /* task1 */
+        byte s = 100;
+        System.out.println("Значение переменной s c типом byte равно " + s);
+        short m = 10000;
+        System.out.println("Значение переменной m c типом short равно " + m);
+        int l = 100000000;
+        System.out.println("Значение переменной l c типом int равно " + l);
+        long xl = 1000000000;
+        System.out.println("Значение переменной xl c типом long равно " + xl);
+        float fraction = 3.14f;
+        System.out.println("Значение переменной fraction c типом float равно " + fraction);
+        double particle = 3.1415926d;
+        System.out.println("Значение переменной particle c типом double равно " + particle);
+
+    }
+}
